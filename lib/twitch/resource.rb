7e247911195ccc1313ec804d74a4ae61d2dee30f
Dean Perry <dean@deanpcmad.com>
2021-09-05 22:23:12 +0100

    added error numbers to message

--- lib/twitch/resource.rb
+++ lib/twitch/resource.rb
@@ -31,21 +31,21 @@ module Twitch
     def handle_response(response)
       case response.status
       when 400
-        raise Error, "Your request was malformed. '#{response.body["message"]}'"
+        raise Error, "Error 400: Your request was malformed. '#{response.body["message"]}'"
       when 401
-        raise Error, "You did not supply valid authentication credentials. '#{response.body["error"]}'"
+        raise Error, "Error 401: You did not supply valid authentication credentials. '#{response.body["error"]}'"
       when 403
-        raise Error, "You are not allowed to perform that action. '#{response.body["error"]}'"
+        raise Error, "Error 403: You are not allowed to perform that action. '#{response.body["error"]}'"
       when 404
-        raise Error, "No results were found for your request. '#{response.body["error"]}'"
+        raise Error, "Error 404: No results were found for your request. '#{response.body["error"]}'"
       when 409
-        raise Error, "Your request was a conflict. '#{response.body["message"]}'"
+        raise Error, "Error 409: Your request was a conflict. '#{response.body["message"]}'"
       when 429
-        raise Error, "Your request exceeded the API rate limit. '#{response.body["error"]}'"
+        raise Error, "Error 429: Your request exceeded the API rate limit. '#{response.body["error"]}'"
       when 500
-        raise Error, "We were unable to perform the request due to server-side problems. '#{response.body["error"]}'"
+        raise Error, "Error 500: We were unable to perform the request due to server-side problems. '#{response.body["error"]}'"
       when 503
-        raise Error, "You have been rate limited for sending more than 20 requests per second. '#{response.body["error"]}'"
+        raise Error, "Error 503: You have been rate limited for sending more than 20 requests per second. '#{response.body["error"]}'"
       when 204
         # 204 is a response for success on Twitch's API
         return true
