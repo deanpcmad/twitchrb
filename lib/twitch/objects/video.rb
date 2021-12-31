a2169ae7a9fe89cd35e5e05b8f3886e1766e694e
Dean Perry <dean@deanpcmad.com>
2021-12-31 16:49:03 +0000

    bug fix for if the there is no thumbnail url

--- lib/twitch/objects/video.rb
+++ lib/twitch/objects/video.rb
@@ -11,11 +11,15 @@ module Twitch
 
     # Generates the Large Thumbnail image URL
     def generate_thumbnail_url_large
+      return "" if thumbnail_url.empty?
+
       thumbnail_url.gsub("%{width}", "640").gsub("%{height}", "360")
     end
   
     # Generates a Storyboard/Animated image URL
     def generate_animated_url
+      return "" if thumbnail_url.empty?
+      
       thumb = thumbnail_url.dup
       thumb.gsub!("https://static-cdn.jtvnw.net/cf_vods/", "").gsub!("/thumb/thumb0-%{width}x%{height}.jpg", "storyboards/#{id}-strip-0.jpg")
       split = thumb.split("/")
