a83a834f0657eacd9164d4c8404f24efe0779190
Dean Perry <dean@deanpcmad.com>
2022-02-23 11:18:51 +0000

    upgrading to faraday 2

--- lib/twitch/client.rb
+++ lib/twitch/client.rb
@@ -116,7 +116,6 @@ module Twitch
         conn.headers = { "Client-ID": client_id }
         conn.request :json
 
-        conn.response :dates
         conn.response :json, content_type: "application/json"
 
         conn.adapter adapter, @stubs
