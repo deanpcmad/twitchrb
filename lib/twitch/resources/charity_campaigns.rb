cdcf913ab8ddf331d4d22c172081ba4315413777
Dean Perry <deanperry@fastmail.com>
2022-10-02 22:00:56 +0100

    return nil if no charity

--- lib/twitch/resources/charity_campaigns.rb
+++ lib/twitch/resources/charity_campaigns.rb
@@ -5,7 +5,11 @@ module Twitch
     # Broadcaster ID must match the user in the OAuth token
     def list(broadcaster_id:)
       response = get_request("charity/campaigns?broadcaster_id=#{broadcaster_id}")     
-      CharityCampaign.new(response.body.dig("data")[0]) if response.success?
+      if response.body.dig("data")[0]
+        CharityCampaign.new(response.body.dig("data")[0])
+      else
+        nil
+      end
     end
 
   end
