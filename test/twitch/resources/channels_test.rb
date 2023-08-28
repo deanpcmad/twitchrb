be10ffc847c67252c0184726a5d26c5074eb7b6b
Dean Perry <deanperry@fastmail.com>
2023-08-28 20:37:02 +0100

    fix tests

--- test/twitch/resources/channels_test.rb
+++ test/twitch/resources/channels_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class ChannelsResourceTest < Minitest::Test
-  
-  def test_channels_get
+
+  def test_channels_retrieve
     stub = stub_request("channels", response: stub_response(fixture: "channels/get"))
     client  = Twitch::Client.new(client_id: "123", access_token: "abc123", adapter: :test, stubs: stub)
-    channel = client.channels.get(broadcaster_id: 141981764)
+    channel = client.channels.retrieve(id: 141981764)
 
     assert_equal Twitch::Channel, channel.class
     assert_equal "twitchdev", channel.broadcaster_login
--- test/twitch/resources/users_test.rb
+++ test/twitch/resources/users_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class UsersResourceTest < Minitest::Test
 
-  def test_users_get_by_id
+  def test_users_retrieve_by_id
     client = set_client(stub_request("users", response: stub_response(fixture: "users/get")))
-    user   = client.users.get_by_id(user_id: 141981764)
+    user   = client.users.retrieve(id: 141981764)
 
     assert_equal Twitch::User, user.class
     assert_equal "twitchdev", user.login
   end
 
-  def test_users_get_by_username
+  def test_users_retrieve_by_username
     client = set_client(stub_request("users", response: stub_response(fixture: "users/get")))
-    user   = client.users.get_by_username(username: "twitchdev")
+    user   = client.users.retrieve(username: "twitchdev")
 
     assert_equal Twitch::User, user.class
     assert_equal "partner", user.broadcaster_type
