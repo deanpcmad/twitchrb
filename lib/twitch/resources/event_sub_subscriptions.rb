022253d6e84485f3421dbc12737d79892d697f92
Dean Perry <deanperry@fastmail.com>
2024-02-04 14:10:19 +0000

    support sending additional params to eventsub sub create

--- lib/twitch/resources/event_sub_subscriptions.rb
+++ lib/twitch/resources/event_sub_subscriptions.rb
@@ -1,13 +1,13 @@
 module Twitch
   class EventSubSubscriptionsResource < Resource
-    
+
     def list(**params)
       response = get_request("eventsub/subscriptions", params: params)
       Collection.from_response(response, type: EventSubSubscription)
     end
 
-    def create(type:, version:, condition:, transport:)
-      attributes = {type: type, version: version, condition: condition, transport: transport}
+    def create(type:, version:, condition:, transport:, **params)
+      attributes = {type: type, version: version, condition: condition, transport: transport}.merge(params)
       response = post_request("eventsub/subscriptions", body: attributes)
 
       EventSubSubscription.new(response.body.dig("data")[0]) if response.success?
