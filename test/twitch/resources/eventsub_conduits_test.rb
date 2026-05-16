d3a00c28fa5994baa9396c991dace76ff22cd1f5
Dean Perry <deanperry@fastmail.com>
2026-05-16 12:40:50 +0100

    and moved eventsub to webmock too

--- test/twitch/resources/eventsub_conduits_test.rb
+++ test/twitch/resources/eventsub_conduits_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
-class EventsubConduitsResourceTest < Minitest::Test
+class EventsubConduitsResourceTest < WebmockTest
+  def setup
+    @client = Twitch::Client.new(client_id: "test_client_id", access_token: "test_user_token")
+    @app_client = Twitch::Client.new(client_id: "test_client_id", access_token: "test_app_token")
+  end
+
   def test_eventsub_conduits_list_with_user_token_fails
-    setup_client
+    stub_request(:get, "#{HELIX_URL}/eventsub/conduits")
+      .to_return(status: 401, body: unauthorized_body, headers: json_headers)
 
     assert_raises(Twitch::Errors::AuthenticationMissingError) do
       @client.eventsub_conduits.list
@@ -10,17 +16,18 @@ class EventsubConduitsResourceTest < Minitest::Test
   end
 
   def test_eventsub_conduits_list_with_app_token
-    app_client = setup_app_client
-    skip "Could not create app client" unless app_client
+    stub_request(:get, "#{HELIX_URL}/eventsub/conduits")
+      .to_return(status: 200, body: helix_fixture("get_conduits"), headers: json_headers)
 
-    conduits = app_client.eventsub_conduits.list
+    conduits = @app_client.eventsub_conduits.list
 
     assert_equal Twitch::Collection, conduits.class
     assert conduits.data.all? { |conduit| conduit.is_a?(Twitch::EventsubConduit) }
   end
 
   def test_eventsub_conduits_create_with_user_token_fails
-    setup_client
+    stub_request(:post, "#{HELIX_URL}/eventsub/conduits")
+      .to_return(status: 401, body: unauthorized_body, headers: json_headers)
 
     assert_raises(Twitch::Errors::AuthenticationMissingError) do
       @client.eventsub_conduits.create(shard_count: 1)
@@ -28,64 +35,92 @@ class EventsubConduitsResourceTest < Minitest::Test
   end
 
   def test_eventsub_conduits_create_with_app_token
-    app_client = setup_app_client
-    skip "Could not create app client" unless app_client
+    conduit_id = "26b1c993-bfcf-44d9-b876-379dacafe75a"
 
-    conduit = app_client.eventsub_conduits.create(shard_count: 1)
+    stub_request(:post, "#{HELIX_URL}/eventsub/conduits")
+      .with(body: hash_including("shard_count" => 1))
+      .to_return(
+        status: 200,
+        body: { data: [ { id: conduit_id, shard_count: 1 } ] }.to_json,
+        headers: json_headers
+      )
 
-    if conduit
-      assert_equal Twitch::EventsubConduit, conduit.class
-      refute_nil conduit.id
-      assert_equal 1, conduit.shard_count
+    stub_request(:delete, "#{HELIX_URL}/eventsub/conduits")
+      .with(query: { "id" => conduit_id })
+      .to_return(status: 204, body: "")
 
-      # Clean up - delete the conduit
-      app_client.eventsub_conduits.delete(id: conduit.id)
-    end
+    conduit = @app_client.eventsub_conduits.create(shard_count: 1)
+
+    assert_equal Twitch::EventsubConduit, conduit.class
+    assert_equal conduit_id, conduit.id
+    assert_equal 1, conduit.shard_count
+
+    @app_client.eventsub_conduits.delete(id: conduit.id)
   end
 
   def test_eventsub_conduits_create_and_manage_lifecycle_with_app_token
-    app_client = setup_app_client
-    skip "Could not create app client" unless app_client
+    conduit_id = "26b1c993-bfcf-44d9-b876-379dacafe75a"
 
-    # Create conduit with multiple shards
-    conduit = app_client.eventsub_conduits.create(shard_count: 2)
-    skip "Could not create conduit" unless conduit
+    stub_request(:post, "#{HELIX_URL}/eventsub/conduits")
+      .with(body: hash_including("shard_count" => 2))
+      .to_return(
+        status: 200,
+        body: { data: [ { id: conduit_id, shard_count: 2 } ] }.to_json,
+        headers: json_headers
+      )
 
-    assert_equal Twitch::EventsubConduit, conduit.class
+    stub_request(:get, "#{HELIX_URL}/eventsub/conduits/shards")
+      .with(query: { "conduit_id" => conduit_id })
+      .to_return(status: 200, body: helix_fixture("get_conduit_shards"), headers: json_headers)
+
+    stub_request(:patch, "#{HELIX_URL}/eventsub/conduits")
+      .with(body: hash_including("id" => conduit_id, "shard_count" => 3))
+      .to_return(
+        status: 200,
+        body: { data: [ { id: conduit_id, shard_count: 3 } ] }.to_json,
+        headers: json_headers
+      )
+
+    stub_request(:delete, "#{HELIX_URL}/eventsub/conduits")
+      .with(query: { "id" => conduit_id })
+      .to_return(status: 204, body: "")
+
+    conduit = @app_client.eventsub_conduits.create(shard_count: 2)
     assert_equal 2, conduit.shard_count
 
-    # Test getting shards
-    shards = app_client.eventsub_conduits.shards(id: conduit.id)
+    shards = @app_client.eventsub_conduits.shards(id: conduit.id)
     assert_equal Twitch::Collection, shards.class
     assert shards.data.all? { |shard| shard.is_a?(Twitch::EventsubConduitShard) }
 
-    # Test updating conduit
-    updated_conduit = app_client.eventsub_conduits.update(id: conduit.id, shard_count: 3)
-    if updated_conduit
-      assert_equal 3, updated_conduit.shard_count
-    end
+    updated = @app_client.eventsub_conduits.update(id: conduit.id, shard_count: 3)
+    assert_equal 3, updated.shard_count
 
-    # Clean up - delete the conduit
-    app_client.eventsub_conduits.delete(id: conduit.id)
+    @app_client.eventsub_conduits.delete(id: conduit.id)
   end
 
   def test_eventsub_conduits_operations_with_user_token_fail
-    setup_client
+    stub_request(:post, "#{HELIX_URL}/eventsub/conduits")
+      .to_return(status: 401, body: unauthorized_body, headers: json_headers)
+    stub_request(:patch, "#{HELIX_URL}/eventsub/conduits")
+      .to_return(status: 401, body: unauthorized_body, headers: json_headers)
+    stub_request(:get, %r{#{HELIX_URL}/eventsub/conduits/shards})
+      .to_return(status: 401, body: unauthorized_body, headers: json_headers)
+    stub_request(:delete, %r{#{HELIX_URL}/eventsub/conduits\?})
+      .to_return(status: 401, body: unauthorized_body, headers: json_headers)
 
-    assert_raises(Twitch::Errors::AuthenticationMissingError) do
-      @client.eventsub_conduits.create(shard_count: 3)
-    end
+    assert_raises(Twitch::Errors::AuthenticationMissingError) { @client.eventsub_conduits.create(shard_count: 3) }
+    assert_raises(Twitch::Errors::AuthenticationMissingError) { @client.eventsub_conduits.update(id: "fake-id", shard_count: 2) }
+    assert_raises(Twitch::Errors::AuthenticationMissingError) { @client.eventsub_conduits.shards(id: "fake-id") }
+    assert_raises(Twitch::Errors::AuthenticationMissingError) { @client.eventsub_conduits.delete(id: "fake-id") }
+  end
 
-    assert_raises(Twitch::Errors::AuthenticationMissingError) do
-      @client.eventsub_conduits.update(id: "fake-id", shard_count: 2)
-    end
+  private
 
-    assert_raises(Twitch::Errors::AuthenticationMissingError) do
-      @client.eventsub_conduits.shards(id: "fake-id")
-    end
+  def json_headers
+    { "Content-Type" => "application/json" }
+  end
 
-    assert_raises(Twitch::Errors::AuthenticationMissingError) do
-      @client.eventsub_conduits.delete(id: "fake-id")
-    end
+  def unauthorized_body
+    { error: "Unauthorized", status: 401, message: "OAuth token is missing" }.to_json
   end
 end
--- test/twitch/resources/eventsub_subscriptions_test.rb
+++ test/twitch/resources/eventsub_subscriptions_test.rb
@@ -1,238 +1,248 @@
 require "test_helper"
 
-class EventsubSubscriptionsResourceTest < Minitest::Test
-  def test_eventsub_subscriptions_list_with_user_token
-    setup_client
-    subscriptions = @client.eventsub_subscriptions.list
+class EventsubSubscriptionsResourceTest < WebmockTest
+  SUBS_URL = "#{HELIX_URL}/eventsub/subscriptions".freeze
 
-    assert_equal Twitch::Collection, subscriptions.class
-    assert subscriptions.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
+  def setup
+    @client = Twitch::Client.new(client_id: "test_client_id", access_token: "test_user_token")
+    @app_client = Twitch::Client.new(client_id: "test_client_id", access_token: "test_app_token")
+  end
+
+  def test_eventsub_subscriptions_list_with_user_token
+    stub_request(:get, SUBS_URL)
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
+
+    subs = @client.eventsub_subscriptions.list
+
+    assert_equal Twitch::Collection, subs.class
+    assert subs.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
   end
 
   def test_eventsub_subscriptions_list_with_app_token
-    app_client = setup_app_client
-    skip "Could not create app client" unless app_client
+    stub_request(:get, SUBS_URL)
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
 
-    subscriptions = app_client.eventsub_subscriptions.list
+    subs = @app_client.eventsub_subscriptions.list
 
-    assert_equal Twitch::Collection, subscriptions.class
-    assert subscriptions.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
+    assert_equal Twitch::Collection, subs.class
+    assert subs.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
   end
 
   def test_eventsub_subscriptions_list_with_status
-    setup_client
-    subscriptions = @client.eventsub_subscriptions.list(status: "enabled")
+    stub_request(:get, SUBS_URL)
+      .with(query: { "status" => "enabled" })
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
 
-    assert_equal Twitch::Collection, subscriptions.class
-    assert subscriptions.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
+    subs = @client.eventsub_subscriptions.list(status: "enabled")
+
+    assert_equal Twitch::Collection, subs.class
+    assert subs.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
   end
 
   def test_eventsub_subscriptions_list_with_type
-    setup_client
-    subscriptions = @client.eventsub_subscriptions.list(type: "channel.update")
+    stub_request(:get, SUBS_URL)
+      .with(query: { "type" => "channel.update" })
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
 
-    assert_equal Twitch::Collection, subscriptions.class
-    assert subscriptions.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
+    subs = @client.eventsub_subscriptions.list(type: "channel.update")
+
+    assert_equal Twitch::Collection, subs.class
+    assert subs.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
   end
 
   def test_eventsub_subscriptions_list_with_user_id
-    setup_client
-    subscriptions = @client.eventsub_subscriptions.list(user_id: "141981764")
+    stub_request(:get, SUBS_URL)
+      .with(query: { "user_id" => "141981764" })
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
 
-    assert_equal Twitch::Collection, subscriptions.class
-    assert subscriptions.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
+    subs = @client.eventsub_subscriptions.list(user_id: "141981764")
+
+    assert_equal Twitch::Collection, subs.class
+    assert subs.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
   end
 
   def test_eventsub_subscriptions_list_with_conduit_id
-    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.get("https://api.twitch.tv/helix/eventsub/subscriptions") do |env|
-        assert_equal({ "conduit_id" => "conduit-123" }, env.params)
+    stub_request(:get, SUBS_URL)
+      .with(query: { "conduit_id" => "conduit-123" })
+      .to_return(
+        status: 200,
+        body: {
+          total: 1,
+          data: [
+            {
+              id: "sub-1",
+              status: "enabled",
+              type: "channel.chat.message",
+              version: "1",
+              condition: { broadcaster_user_id: "123", user_id: "321" },
+              transport: { method: "conduit", conduit_id: "conduit-123" },
+              cost: 0,
+              created_at: "2026-04-17T12:00:00Z"
+            }
+          ],
+          pagination: {}
+        }.to_json,
+        headers: json_headers
+      )
 
-        [
-          200,
-          { "content-type" => "application/json" },
-          JSON.dump(
-            total: 1,
-            data: [
-              {
-                id: "sub-1",
-                status: "enabled",
-                type: "channel.chat.message",
-                version: "1",
-                condition: { broadcaster_user_id: "123", user_id: "321" },
-                transport: { method: "conduit", conduit_id: "conduit-123" },
-                cost: 0,
-                created_at: "2026-04-17T12:00:00Z"
-              }
-            ],
-            pagination: {}
-          )
-        ]
-      end
-    end
+    subs = @client.eventsub_subscriptions.list(conduit_id: "conduit-123")
 
-    client = Twitch::Client.new(client_id: "123", access_token: "abc123", adapter: :test)
-    client.instance_variable_set(:@stubs, stubs)
-
-    subscriptions = client.eventsub_subscriptions.list(conduit_id: "conduit-123")
-
-    assert_equal Twitch::Collection, subscriptions.class
-    assert_equal "sub-1", subscriptions.first.id
+    assert_equal Twitch::Collection, subs.class
+    assert_equal "sub-1", subs.first.id
   end
 
   def test_eventsub_subscriptions_list_with_pagination
-    setup_client
+    stub_request(:get, SUBS_URL)
+      .with(query: { "first" => "5" })
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
+
+    stub_request(:get, SUBS_URL)
+      .with(query: { "first" => "5", "after" => "eyJiIjpudWxsLCJhIjp7Ik9mZnNldCI6MX19" })
+      .to_return(status: 200, body: helix_fixture("get_eventsub_subscriptions"), headers: json_headers)
 
     first_page = @client.eventsub_subscriptions.list(first: 5)
+    assert first_page.cursor, "expected pagination cursor in fixture"
 
-    if first_page.cursor
-      subscriptions = @client.eventsub_subscriptions.list(first: 5, after: first_page.cursor)
-
-      assert_equal Twitch::Collection, subscriptions.class
-      assert subscriptions.data.count <= 5
-      assert subscriptions.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
-    else
-      assert_equal Twitch::Collection, first_page.class
-      assert first_page.data.count <= 5
-      assert first_page.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
-    end
+    page2 = @client.eventsub_subscriptions.list(first: 5, after: first_page.cursor)
+    assert_equal Twitch::Collection, page2.class
+    assert page2.data.all? { |sub| sub.is_a?(Twitch::EventsubSubscription) }
   end
 
   def test_eventsub_subscriptions_create_webhook_with_user_token_fails
-    setup_client
+    stub_request(:post, SUBS_URL)
+      .to_return(
+        status: 400,
+        body: { error: "Bad Request", status: 400, message: "webhook transport requires app token" }.to_json,
+        headers: json_headers
+      )
 
     assert_raises(Twitch::Errors::BadRequestError) do
       @client.eventsub_subscriptions.create(
         type: "channel.update",
         version: "1",
-        condition: {
-          broadcaster_user_id: "141981764"
-        },
-        transport: {
-          method: "webhook",
-          callback: "https://example.com/webhooks/callback",
-          secret: "secretkey123"
-        }
+        condition: { broadcaster_user_id: "141981764" },
+        transport: { method: "webhook", callback: "https://example.com/webhooks/callback", secret: "secretkey123" }
       )
     end
   end
 
   def test_eventsub_subscriptions_create_webhook_with_app_token
-    app_client = setup_app_client
-    skip "Could not create app client" unless app_client
+    stub_request(:post, SUBS_URL)
+      .with(body: hash_including("type" => "channel.update", "version" => "1"))
+      .to_return(
+        status: 202,
+        body: {
+          data: [
+            {
+              id: "new-sub-id",
+              status: "webhook_callback_verification_pending",
+              type: "channel.update",
+              version: "1",
+              condition: { broadcaster_user_id: "141981764" },
+              created_at: "2025-04-10T12:00:00Z",
+              transport: { method: "webhook", callback: "https://example.com/webhooks/callback" },
+              cost: 1
+            }
+          ],
+          total: 1,
+          total_cost: 1,
+          max_total_cost: 10000
+        }.to_json,
+        headers: json_headers
+      )
 
-    subscription = app_client.eventsub_subscriptions.create(
+    subscription = @app_client.eventsub_subscriptions.create(
       type: "channel.update",
       version: "1",
-      condition: {
-        broadcaster_user_id: "141981764"
-      },
-      transport: {
-        method: "webhook",
-        callback: "https://example.com/webhooks/callback",
-        secret: "secretkey123"
-      }
+      condition: { broadcaster_user_id: "141981764" },
+      transport: { method: "webhook", callback: "https://example.com/webhooks/callback", secret: "secretkey123" }
     )
 
-    if subscription
-      assert_equal Twitch::EventsubSubscription, subscription.class
-      refute_nil subscription.id
-      assert_equal "channel.update", subscription.type
-      assert_equal "1", subscription.version
-    end
+    assert_equal Twitch::EventsubSubscription, subscription.class
+    assert_equal "new-sub-id", subscription.id
+    assert_equal "channel.update", subscription.type
+    assert_equal "1", subscription.version
   end
 
   def test_eventsub_subscriptions_create_websocket_requires_valid_session
-    setup_client
+    stub_request(:post, SUBS_URL)
+      .to_return(
+        status: 400,
+        body: { error: "Bad Request", status: 400, message: "invalid websocket session id" }.to_json,
+        headers: json_headers
+      )
 
     assert_raises(Twitch::Errors::BadRequestError) do
       @client.eventsub_subscriptions.create(
         type: "channel.update",
         version: "1",
-        condition: {
-          broadcaster_user_id: "141981764"
-        },
-        transport: {
-          method: "websocket",
-          session_id: "invalid-session-id"
-        }
+        condition: { broadcaster_user_id: "141981764" },
+        transport: { method: "websocket", session_id: "invalid-session-id" }
       )
     end
   end
 
   def test_eventsub_subscriptions_delete
-    setup_client
+    stub_request(:delete, SUBS_URL)
+      .with(query: { "id" => "non-existent-id" })
+      .to_return(
+        status: 404,
+        body: { error: "Not Found", status: 404, message: "subscription not found" }.to_json,
+        headers: json_headers
+      )
 
-    # First try to get an existing subscription to delete
-    subscriptions = @client.eventsub_subscriptions.list(first: 1)
-
-    if subscriptions.data.any?
-      subscription_id = subscriptions.data.first.id
-      response = @client.eventsub_subscriptions.delete(id: subscription_id)
-
-      assert_respond_to response, :status
-    else
-      # Test with a mock ID to verify the endpoint works (expect 404 for non-existent)
-      assert_raises(Twitch::Errors::EntityNotFoundError) do
-        @client.eventsub_subscriptions.delete(id: "non-existent-id")
-      end
+    assert_raises(Twitch::Errors::EntityNotFoundError) do
+      @client.eventsub_subscriptions.delete(id: "non-existent-id")
     end
   end
 
   def test_eventsub_subscriptions_create_invalid_transport_raises_error
-    setup_client
+    stub_request(:post, SUBS_URL)
+      .to_return(
+        status: 400,
+        body: { error: "Bad Request", status: 400, message: "transport method must be webhook, websocket, or conduit" }.to_json,
+        headers: json_headers
+      )
 
     assert_raises(Twitch::Errors::BadRequestError) do
       @client.eventsub_subscriptions.create(
         type: "channel.update",
         version: "1",
-        condition: {
-          broadcaster_user_id: "141981764"
-        },
-        transport: {
-          method: "invalid_method"
-        }
+        condition: { broadcaster_user_id: "141981764" },
+        transport: { method: "invalid_method" }
       )
     end
   end
 
   def test_eventsub_subscriptions_create_conflict_exposes_existing_subscription_id
-    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.post("https://api.twitch.tv/helix/eventsub/subscriptions") do |_env|
-        [
-          409,
-          { "content-type" => "application/json" },
-          JSON.dump(
-            error: "Conflict",
-            status: 409,
-            message: "subscription already exists",
-            data: [
-              { id: "existing-subscription-id" }
-            ]
-          )
-        ]
-      end
-    end
-
-    client = Twitch::Client.new(client_id: "123", access_token: "abc123", adapter: :test)
-    client.instance_variable_set(:@stubs, stubs)
+    stub_request(:post, SUBS_URL)
+      .to_return(
+        status: 409,
+        body: {
+          error: "Conflict",
+          status: 409,
+          message: "subscription already exists",
+          data: [ { id: "existing-subscription-id" } ]
+        }.to_json,
+        headers: json_headers
+      )
 
     error = assert_raises(Twitch::Errors::EventsubSubscriptionConflictError) do
-      client.eventsub_subscriptions.create(
+      @client.eventsub_subscriptions.create(
         type: "channel.update",
         version: "1",
-        condition: {
-          broadcaster_user_id: "141981764"
-        },
-        transport: {
-          method: "webhook",
-          callback: "https://example.com/webhooks/callback",
-          secret: "secretkey123"
-        }
+        condition: { broadcaster_user_id: "141981764" },
+        transport: { method: "webhook", callback: "https://example.com/webhooks/callback", secret: "secretkey123" }
       )
     end
 
     assert_equal "existing-subscription-id", error.existing_subscription_id
     assert_equal "subscription already exists", error.twitch_error_message
+  end
+
+  private
+
+  def json_headers
+    { "Content-Type" => "application/json" }
   end
 end
