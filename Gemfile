eb22adf22842c50f87102b905f47283723b59294
Dean Perry <deanperry@fastmail.com>
2026-05-16 12:48:57 +0100

    remove vcr & cassettes

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gemspec
 gem "rake", "~> 12.0"
 gem "minitest", "~> 5.0"
 gem "dotenv"
-gem "vcr"
 gem "webmock"
 gem "rubocop-rails-omakase", require: false
 gem "activesupport", ">= 4.2.0", "< 8.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,7 @@ require "twitch"
 require "minitest/autorun"
 require "faraday"
 require "json"
-require "vcr"
 require "webmock/minitest"
-require "dotenv/load"
 
 WebMock.disable_net_connect!
 
@@ -24,56 +22,7 @@ def stub_helix(method, path, query: nil, fixture: nil, status: 200, body: nil)
   stub.to_return(status: status, body: body, headers: { "Content-Type" => "application/json" })
 end
 
-# Tests that opt out of VCR (WebMock-based) inherit from this class.
+# Base class for HTTP-stubbed tests. Equivalent to Minitest::Test now that VCR is gone;
+# kept as a distinct name so the WebMock pattern is obvious at test sites.
 class WebmockTest < Minitest::Test
-  def setup; end
-  def teardown; end
-end
-
-VCR.configure do |config|
-  config.cassette_library_dir = "test/vcr_cassettes"
-  config.hook_into :faraday
-  # When no cassette is active, hand the request off to WebMock (used by stub-based tests).
-  config.allow_http_connections_when_no_cassette = true
-
-  config.filter_sensitive_data("<AUTHORIZATION>") { ENV["TWITCH_ACCESS_TOKEN"] }
-  config.filter_sensitive_data("<CLIENT_SECRET>") { ENV["TWITCH_CLIENT_SECRET"] }
-  config.filter_sensitive_data("<CLIENT_ID>") { ENV["TWITCH_CLIENT_ID"] }
-  config.filter_sensitive_data("<APP_ACCESS_TOKEN>") do |interaction|
-    # Filter any dynamically generated app access tokens
-    if interaction.response&.body&.include?("access_token")
-      begin
-        body = JSON.parse(interaction.response.body)
-        body["access_token"] if body.is_a?(Hash)
-      rescue
-        nil
-      end
-    end
-  end
-
-  # Allow HTTP connections for OAuth endpoints since they work differently
-  config.ignore_request do |request|
-    request.uri.include?("id.twitch.tv")
-  end
-end
-
-def setup_client
-  @client ||= Twitch::Client.new(client_id: ENV["TWITCH_CLIENT_ID"], access_token: ENV["TWITCH_ACCESS_TOKEN"])
-end
-
-def setup_app_client
-  @app_client ||= Twitch::Client.new(
-    client_id: ENV.fetch("TWITCH_CLIENT_ID", "test_client_id"),
-    access_token: "test_app_token"
-  )
-end
-
-class Minitest::Test
-  def setup
-    VCR.insert_cassette(name)
-  end
-
-  def teardown
-    VCR.eject_cassette
-  end
 end
